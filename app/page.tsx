019be6c153f0a42031077158c92171c1a3afe91e
Eric Stevens Jr. <ericstevensjrprofessional@outlook.com>
2026-07-07 23:04:56 -0400

    This is me showing that I know the Git pipeline so we can see where I'm screwing up all the code.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <main style={{ padding: "4rem", fontFamily: "sans-serif"}}>
-    <h1>Day 1.</h1>
+    <h1>This is my own headline for day 1 to show that I know what the hell I'm doing in live development.</h1>
     <p>Shipped July 7, 2026. Everything starts here.</p>
    </main>
   );
